7b2ddf0cdcdaea9ec8ee6ad573d9f3887a8ce595
Agust-C <81574275+raishai@users.noreply.github.com>
2022-03-15 12:11:53 -0400

    Update db-inventory.sql

--- db-inventory.sql
+++ db-inventory.sql
@@ -8,47 +8,44 @@ CREATE TABLE `merch` (
   `game_name` varchar(50) NOT NULL,
   `game_console` varchar(50) NOT NULL,
   `game_genre` varchar(50) NOT NULL,
-  `game_seller` varchar(50) NOT NULL,
+  `game_seller` int(5) NOT NULL,
   `unit_price` decimal(4,2) NOT NULL,
   PRIMARY KEY (`product_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-INSERT INTO `merch` VALUES (1,'Animal Crossing','Nintendo','Adventure', 3,49.95);
-INSERT INTO `merch` VALUES (2,'Super Smash Bros','Nintendo','Adventure'49,4.65);
-INSERT INTO `merch` VALUES (3,'Pokemon Legends Arceus','Nintendo','Adventure',38,3.35);
-INSERT INTO `merch` VALUES (4,'Luigis Mansion','Nintendo','Adventure',90,4.53);
-INSERT INTO `merch` VALUES (5,'Mario Kart 8','Nintendo','Adventure',94,1.63);
-INSERT INTO `merch` VALUES (6,'Breath of the Wild','Nintendo','Adventure',14,2.39);
-INSERT INTO `merch` VALUES (7,'Super Mario 3D World','Nintendo','Adventure',98,3.29);
-INSERT INTO `merch` VALUES (8,'Splatoon 2','Nintendo','Adventure',26,0.74);
-INSERT INTO `merch` VALUES (9,'Kirby and the Forgotten Land','Nintendo','Adventure',67,2.26);
-INSERT INTO `merch` VALUES (10,'Mario Party Superstars','Nintendo','Adventure',6,1.09);
+INSERT INTO `merch` VALUES (1,'Animal Crossing','Nintendo','Adventure',3,49.95);
+INSERT INTO `merch` VALUES (2,'Super Smash Bros','Nintendo','Party',2,34.99);
+INSERT INTO `merch` VALUES (3,'Pokemon Legends Arceus','Nintendo','Action',1,34.95);
+INSERT INTO `merch` VALUES (4,'Luigis Mansion','Nintendo','Adventure',4,44.95);
+INSERT INTO `merch` VALUES (5,'Mario Kart 8','Nintendo','Party',4,34.99);
+INSERT INTO `merch` VALUES (6,'Breath of the Wild','Nintendo','Adventure',1,24.99);
+INSERT INTO `merch` VALUES (7,'Super Mario 3D World','Nintendo','Adventure',2,49.99);
+INSERT INTO `merch` VALUES (8,'Splatoon 2','Nintendo','Party',5,34.99);
+INSERT INTO `merch` VALUES (9,'Kirby and the Forgotten Land','Nintendo','Adventure',4,24.99);
+INSERT INTO `merch` VALUES (10,'Mario Party Superstars','Nintendo','Party',2,19.99);
 
-INSERT INTO `merch` VALUES (10,'Sifu','Playstation','Action',6,1.09);
-INSERT INTO `merch` VALUES (10,'Ghost of Tsuhima','Playstation','Action',6,1.09);
-INSERT INTO `merch` VALUES (10,'Elden Ring','Playstation','Action',6,1.09);
-INSERT INTO `merch` VALUES (10,'Kena','Playstation','Action',6,1.09);
-INSERT INTO `merch` VALUES (10,'Hitman 3','Playstation','Action',6,1.09);
-INSERT INTO `merch` VALUES (10,'Demons Souls','Playstation','Action',6,1.09);
-INSERT INTO `merch` VALUES (10,'Hades','Playstation','Action',6,1.09);
-INSERT INTO `merch` VALUES (10,'It Takes Two','Playstation','Action',6,1.09);
-INSERT INTO `merch` VALUES (10,'Dolmen','Playstation','Action',6,1.09);
-INSERT INTO `merch` VALUES (10,'Persona 5','Playstation','Action',6,1.09);
-INSERT INTO `merch` VALUES (10,'Sleeping Dogs','Playstation','Action',6,1.09);
-INSERT INTO `merch` VALUES (10,'God of War','Playstation','Action',6,1.09);
-INSERT INTO `merch` VALUES (10,'Uncharted 4','Playstation','Action',6,1.09);
-INSERT INTO `merch` VALUES (10,'Last of Us','Playstation','Action',6,1.09);
-INSERT INTO `merch` VALUES (10,'NBA 2k22','Playstation','Action',6,1.09);
-INSERT INTO `merch` VALUES (10,'FIFA 22','Playstation','Action',6,1.09);
+INSERT INTO `merch` VALUES (11,'Sifu','Playstation','Action',3,44.99);
+INSERT INTO `merch` VALUES (12,'Ghost of Tsuhima','Playstation','Action',1,24.99);
+INSERT INTO `merch` VALUES (13,'Elden Ring','Playstation','Adventure',4,34.99);
+INSERT INTO `merch` VALUES (14,'Kena','Playstation','Adventure',5,44.99);
+INSERT INTO `merch` VALUES (15,'Hitman 3','Playstation','Action',3,49.99);
+INSERT INTO `merch` VALUES (16,'Demons Souls','Playstation','Adventure',2,54.99);
+INSERT INTO `merch` VALUES (17,'Hades','Playstation','Puzzle',1,24.99);
+INSERT INTO `merch` VALUES (18,'It Takes Two','Playstation','Puzzle',4,19.99);
+INSERT INTO `merch` VALUES (19,'Dolmen','Playstation','Action',2,44.99);
+INSERT INTO `merch` VALUES (20,'Persona 5','Playstation','Adventure',5,34.99);
+INSERT INTO `merch` VALUES (21,'Sleeping Dogs','Playstation','Adventure',4,24.99);
+INSERT INTO `merch` VALUES (22,'God of War','Playstation','Action',3,34.99);
+INSERT INTO `merch` VALUES (23,'Uncharted 4','Playstation','Action',3,24.99);
+INSERT INTO `merch` VALUES (24,'Last of Us','Playstation','Adventure',2,34.99);
+INSERT INTO `merch` VALUES (25,'NBA 2k22','Playstation','Party',1,19.99);
+INSERT INTO `merch` VALUES (26,'FIFA 22','Playstation','Party',4,24.99);
 
-INSERT INTO `merch` VALUES (10,'Halo','Xbox','Action',6,1.09);
-INSERT INTO `merch` VALUES (10,'Fallout 76','Xbox','Action',6,1.09);
-INSERT INTO `merch` VALUES (10,'Flight Simulator','Xbox','Action',6,1.09);
-INSERT INTO `merch` VALUES (10,'Sea of Thieves','Xbox','Action',6,1.09);
-INSERT INTO `merch` VALUES (10,'Minecraft','Xbox','Action',6,1.09);
-INSERT INTO `merch` VALUES (10,'Grand Theft Auto','Xbox','Action',6,1.09);
-INSERT INTO `merch` VALUES (10,'Life is Strange','Xbox','Action',6,1.09);
-INSERT INTO `merch` VALUES (10,'Black Ops 3','Xbox','Action',6,1.09);
-INSERT INTO `merch` VALUES (10,'Sniper Elite 4','Xbox','Action',6,1.09);
-
-
-
+INSERT INTO `merch` VALUES (27,'Halo','Xbox','Action',5,19.99);
+INSERT INTO `merch` VALUES (28,'Fallout 76','Xbox','Action',4,9.99);
+INSERT INTO `merch` VALUES (29,'Flight Simulator','Xbox','Adventure',3,14.99);
+INSERT INTO `merch` VALUES (30,'Sea of Thieves','Xbox','Action',2,14.99);
+INSERT INTO `merch` VALUES (31,'Minecraft','Xbox','Adventure',1,9.99);
+INSERT INTO `merch` VALUES (32,'Grand Theft Auto','Xbox','Action',3,14.99);
+INSERT INTO `merch` VALUES (33,'Life is Strange','Xbox','Adevnture',5,19.99);
+INSERT INTO `merch` VALUES (34,'Black Ops 3','Xbox','Action',2,9.99);
+INSERT INTO `merch` VALUES (35,'Sniper Elite 4','Xbox','Action',2,14.99);
